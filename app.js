06833ea510c6a4ff3d4150dd590a1b9be2ef051c
Mahmoudd4 <mahmoudhossam1101@gmail.com>
2024-07-08 13:44:37 +0300

    edit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
       console.error('Error incrementing counter:', err);
       return res.status(500).send('Error');
     }
-    res.send(`<h1> Hello 4, welcome to my task,Visitor Count: ${count}</h1>`);
+    res.send(`<h1> Hello 5, welcome to my task,Visitor Count: ${count}</h1>`);
   });
 });
 
