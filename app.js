feba24f992d6f1b35e16505b8c72ee6026fd70c7
Mahmoudd4 <mahmoudhossam1101@gmail.com>
2024-07-09 12:41:02 +0300

    edittt

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
       console.error('Error incrementing counter:', err);
       return res.status(500).send('Error');
     }
-    res.send(`<h1> Hello 1003, welcome to my task,Visitor Count: ${count}</h1>`);
+    res.send(`<h1> Hello 1004, welcome to my task,Visitor Count: ${count}</h1>`);
   });
 });
 
